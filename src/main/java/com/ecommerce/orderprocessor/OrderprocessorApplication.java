03e4ad57ba75c597f7a47dff807937b360825129
Saurabh Chavhan <saurabh.chavhan@outlook.com>
2025-08-03 22:54:18 -0500

    init commit

---
+++ src/main/java/com/ecommerce/orderprocessor/OrderprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.orderprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderprocessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/orderprocessor/OrderprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.orderprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
